588df85e7448a3e193c8410fdd235a5effc2ffda
mamatha-ezh <mamatha.ezh@gmail.com>
2022-02-16 15:38:24 +0400

    Added respository classes

---
+++ src/main/java/mkac/sb/recipe/repository/UOMRepository.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mkac.sb.recipe.repository;
+
+import mkac.sb.recipe.domain.UOM;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author mamat
+ */
+public interface UOMRepository extends CrudRepository<UOM,Long>{
+    
+}
