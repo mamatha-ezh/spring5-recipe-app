dc3d6e9b617b5fe9489b35cf2bfb5decc9997f18
mamatha-ezh <mamatha.ezh@gmail.com>
2022-03-07 10:53:05 +0400

    Recipe services with  test: with ArgumentCaptor

--- src/test/java/mkac/sb/recipe/controllers/IndexControllerTest.java
+++ src/test/java/mkac/sb/recipe/controllers/IndexControllerTest.java
@@ -5,15 +5,20 @@
  */
 package mkac.sb.recipe.controllers;
 
+import java.util.HashSet;
+import java.util.Set;
+import mkac.sb.recipe.domain.Recipe;
 import mkac.sb.recipe.service.RecipeServable;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.mockito.ArgumentCaptor;
 import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import static org.mockito.Mockito.when;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
@@ -48,11 +53,22 @@ public class IndexControllerTest {
      */
     @Test
     public void testGetIndex() {
+         Set<Recipe> recipes = new HashSet<>();
+        Recipe r1= new Recipe();    
+        r1.setId(1L);
+        Recipe r2= new Recipe();    
+        r2.setId(2L);
+        recipes.add(r1);
+        recipes.add(r2);
         System.out.println("getIndex");
+        when(recipeServable.getRecipes()).thenReturn(recipes);
+        ArgumentCaptor<Set<Recipe>> ac = ArgumentCaptor.forClass(Set.class);
         String viewName= controller.getIndex(model);
         assertEquals("index", viewName);
+       
+        
         Mockito.verify(recipeServable,Mockito.times(1)).getRecipes();
-        Mockito.verify(model,Mockito.times(1)).addAttribute(eq("recipes"), anySet());
+        Mockito.verify(model,Mockito.times(1)).addAttribute(eq("recipes"), ac.capture());
    
     
     }
